c374d972698b0241d86f89642d4e9bc0d215f23e
resedr <resedr@ttu.ee>
2017-04-13 11:24:53 +0300

    drawing rectangle not working

---
+++ core/assets/map/pine.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pine" tilewidth="256" tileheight="256" tilecount="1" columns="1">
+ <image source="pine-none04.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="suur" value=""/>
+  </properties>
+ </tile>
+</tileset>
