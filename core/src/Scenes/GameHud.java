5c2dd8811bb06aee445e78a43bf45d5bbb972f34
Rasmus <rasikk@ttu.ee>
2017-04-14 14:42:50 +0300

    Starting screen implementation. Minimap working Gamehud update Ingame game menu --> need fix

--- core/src/Scenes/GameHud.java
+++ core/src/Scenes/GameHud.java
@@ -2,6 +2,8 @@ package Scenes;
 
 import Screens.MainScreen;
 
+import character_movement.CharacterMovement;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -9,11 +11,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.FillViewport;
@@ -46,17 +49,29 @@ public class GameHud {
     private Table resourceTable;
     private Table unitActivitiesTable;
     private Table map;
+    private Table menu;
+
+    private Table wood;
+    private Table food;
+    private Table gold;
 
     private Skin unitSkin;
     private Skin resourceSkin;
     private Skin timeSkin;
+    private Skin woodSkin;
+    private Skin foodSkin;
+    private Skin goldSkin;
+    private Skin menuSkin;
 
     private Drawable drawable;
     private Drawable drawable2;
     private Drawable timeBG;
 
-    private Image unit;
+    private final Button menuButton;
+
+    public  Image unit;
     private Image fighterIcon;
+
 
     public GameHud(SpriteBatch spriteBatch) {
 
@@ -79,8 +94,10 @@ public class GameHud {
         resourceSkin = new Skin();
         resourceSkin.add("bg", new Texture("core/assets/screens/gamehud/bg.jpg"));
         drawable2 = resourceSkin.getDrawable("bg");
+
         unit = new Image(new Texture("core/assets/screens/gamehud/testunit.png"));
         unit.setScaling(Scaling.stretch);
+
         fighterIcon = new Image(new Texture("core/assets/screens/gamehud/fighter_icon.bmp"));
         fighterIcon.setScale(0.5f, 0.5f);
 
@@ -93,28 +110,62 @@ public class GameHud {
         //define labels using the String, and a Label style consisting of a font and color
 
         //resources
-        woodLabel = new Label(("Wood: " + woodCount), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
-        foodLabel = new Label(("Food: " + foodCount), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
-        goldLabel = new Label(("Gold: " + goldCount), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
+        wood = new Table();
+        woodSkin = new Skin();
+        woodSkin.add("wood", new Texture("core/assets/screens/gamehud/wood.png"));
+        wood.setBackground(woodSkin.getDrawable("wood"));
+
+        food = new Table();
+        foodSkin = new Skin();
+        foodSkin.add("food", new Texture("core/assets/screens/gamehud/food.png"));
+        food.setBackground(foodSkin.getDrawable("food"));
+
+        gold = new Table();
+        goldSkin = new Skin();
+        goldSkin.add("gold", new Texture("core/assets/screens/gamehud/gold.png"));
+        gold.setBackground(goldSkin.getDrawable("gold"));
+
+        woodLabel = new Label(("    " + woodCount), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
+        foodLabel = new Label(("    " + foodCount), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
+        goldLabel = new Label(("    " + goldCount), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
 
         woodLabel.setFontScale(2);
         foodLabel.setFontScale(2);
         goldLabel.setFontScale(2);
 
+
         countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
         scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.BLACK));
         unitActivitiesLabel = new Label("Selected Unit Activites", new Label.LabelStyle(new BitmapFont(), Color.BLACK));
-
-
 
         unitActivitiesLabel.setFontScale(2);
         scoreLabel.setFontScale(2);
         countdownLabel.setFontScale(2);
 
+        // In game menu button
+        menuSkin = new Skin(Gdx.files.internal("core/assets/screens/startingscreen/uiskin.json"));
+        menuButton = new TextButton("MENU", menuSkin, "default");
+        menuButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Restart clicked!");
+            }
+            //TODO: button location is somewhere lost :p. Need fix!
+        });
+
+        //Menu skin
+        menu = new Table();
+//        menu.setFillParent(true);
+        menu.setPosition(1480, 770);
+        menu.setSize(120, 30);
+        menu.add(menuButton).size(120, 30);
+
+
         // Game time table configuration;
         timeTable = new Table();
+        timeTable.center();
         timeTable.setBackground(timeBG);
-        timeTable.setPosition(1500, 700);
+        timeTable.setPosition(750, 700);
         timeTable.setSize(100, 100);
         timeTable.add(countdownLabel);
 
@@ -125,10 +176,14 @@ public class GameHud {
 
         //Configuring resource table.
         resourceTable = new Table();
+        resourceTable.left();
+        resourceTable.add(wood).size(40, 30).pad(2);
         resourceTable.add(woodLabel);
         resourceTable.row();
+        resourceTable.add(food).size(40, 30).pad(2);
         resourceTable.add(foodLabel);
         resourceTable.row();
+        resourceTable.add(gold).size(40, 30).pad(2);
         resourceTable.add(goldLabel);
 
         // Configuring Selected unit Activities
@@ -155,11 +210,18 @@ public class GameHud {
         //add table to the stage
         stage.addActor(root);
         stage.addActor(timeTable);
+        stage.addActor(menu);
+
+        Gdx.input.setInputProcessor(stage);
     }
 
     public void update(float dt) {
+//        System.out.println(menuButton.getX() + " " + menuButton.getY());
+
         timeCount += dt;
-        woodLabel.setText(String.format("Wood: %d", woodCount));
+        woodLabel.setText(String.format("%d", woodCount));
+        foodLabel.setText(String.format("%d", foodCount));
+        goldLabel.setText(String.format("%d", goldCount));
         if (timeCount >= 1) {
             if (worldTimer > 0) {
                 worldTimer--;
@@ -180,8 +242,6 @@ public class GameHud {
         scoreLabel.setText(String.format("%06d", score));
     }
 
-
-
     public boolean isTimeUp() {
         return timeUp;
     }
@@ -190,13 +250,6 @@ public class GameHud {
         return worldTimer;
     }
 
-    public static Label getScoreLabel() {
-        return scoreLabel;
-    }
-
-    public static Integer getScore() {
-        return score;
-    }
 
     public static void addWood(int amount) {
         woodCount += amount;
